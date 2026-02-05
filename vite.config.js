d4ec63ff3dd1eafce88adbaf5a3bed6f285dcc18
danstonedev <danstone.pt@gmail.com>
2026-02-05 11:34:03 -0600

    chore: add GitHub Pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/scope-or-nope/',
   plugins: [react()],
   test: {
     globals: true,
